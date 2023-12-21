0af1d211a8709f8cbcd2eed0d064a0c57aac4c7e
Natharyu <anthony.dewitte@3wa.io>
2023-12-21 11:09:04 +0100

    add randomiser at home page for have 4 random characters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,43 @@ import Card from "../components/Global/Card";
 import "./home.scss";
 
 function Home() {
-  return <h2>Home</h2>;
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [randomCharacters, setRandomCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      setIsLoading(true);
+      await fetch("https://finalspaceapi.com/api/v0/character/").then((response) => {
+        response.json().then((data) => {
+          setCharacters(data);
+          getRandomCharacters(data);
+          setIsLoading(false);
+        });
+      });
+    };
+    fetchCharacters().catch(console.error);
+  }, []);
+
+  const getRandomCharacters = (data) => {
+    const characters = [];
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      if (!characters.includes(data[randomIndex])) {
+        characters.push(data[randomIndex]);
+        setRandomCharacters(characters);
+      } else {
+        i--;
+      }
+    }
+  };
+
+  return (
+    <section>
+      <h2>Home</h2>
+      <div>{isLoading ? <p>Chargement...</p> : <Card characters={randomCharacters} />}</div>
+    </section>
+  );
 }
 
 export default Home;
