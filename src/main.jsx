166483e25507f860218206e5e1bcca8332ac2692
Natharyu <anthony.dewittelebrun@gmail.com>
2024-01-02 17:05:16 +0100

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import Router from "./Router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SpeedInsights />
+    <Analytics />
     <Provider store={store}>
       <Router />
     </Provider>
