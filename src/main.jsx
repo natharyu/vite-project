0689d83200a3778e43dbc6626b070414fef3cee0
Natharyu <anthony.dewittelebrun@gmail.com>
2024-01-02 17:03:29 +0100

    vercel speedinsight

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import Router from "./Router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <SpeedInsights />
     <Provider store={store}>
       <Router />
     </Provider>
